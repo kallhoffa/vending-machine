1ddb477b1eacccc35c94217286fedda29482e3f2
Anthony Kallhoff <akallhoff@tron-akallhoff-mbp.local>
2020-11-23 11:44:51 -1000

    passes first few tests of deposit

--- src/Machine.js
+++ src/Machine.js
@@ -13,6 +13,17 @@ class Machine {
     }
     this.snacks = inventory
   }
+
+  deposit(amount) {
+    let validDeposits = [10, 20, 50, 100, 500];
+
+    if(validDeposits.includes(amount)) {
+      return `You have deposited ${amount}`;
+    } else {
+      throw Error('Amount not supported, please use 10, 20, 50, 100, or 500');
+    }
+
+  }
 }
 
 module.exports = Machine
