1ff790d14e0b6149217d2c5b99eea3c58d793dd9
Anthony Kallhoff <akallhoff@tron-akallhoff-mbp.local>
2020-11-23 12:54:08 -1000

    adds selectitem code

--- src/Machine.js
+++ src/Machine.js
@@ -1,6 +1,7 @@
 class Machine {
   constructor() {
     this.snacks = []
+    this.deposited = 0
   }
 
   seeSelections() {
@@ -18,11 +19,25 @@ class Machine {
     let validDeposits = [10, 20, 50, 100, 500];
 
     if(validDeposits.includes(amount)) {
-      return `You have deposited ${amount}`;
+      this.deposited += amount
+      return `You have deposited ${this.deposited}`;
     } else {
       throw Error('Amount not supported, please use 10, 20, 50, 100, or 500');
     }
 
+  }
+
+  selectItem(item) {
+
+    if (this.snacks.filter((snack) => snack.name === item).length === 0) {
+      return 'The item you selected is unavailable'
+    } else {
+      if (this.deposited > item.price) {
+        return "Here's your item.";
+      } else {
+        throw Error(`Your deposit is insufficient.  Please add ${(item.price - this.deposited)} for this item`);
+      }
+    }  
   }
 }
 
--- test/Machine.test.js
+++ test/Machine.test.js
@@ -64,8 +64,8 @@ describe('The vending machine', () => {
 
   it('accepts an amount of money', () => {
     const vendingMachine = new Machine();
-    const depositAmount = 100;
-    const feedback = 'You have deposited 100';
+    const depositAmount = 10;
+    const feedback = 'You have deposited 10';
 
     expect(vendingMachine.deposit(depositAmount)).toEqual(feedback);
   });
@@ -74,8 +74,14 @@ describe('The vending machine', () => {
     const vendingMachine = new Machine()
     const displayMessage = 'Amount not supported, please use 10, 20, 50, 100, or 500'
 
-    expect(() => vendingMachine.deposit(-1).toThrow(displayMessage))
+    expect(() => vendingMachine.deposit(0)).toThrow(displayMessage)
+    expect(() => vendingMachine.deposit(-1)).toThrow(displayMessage)
+    expect(() => vendingMachine.deposit()).toThrow(displayMessage)
+    expect(() => vendingMachine.deposit('test')).toThrow(displayMessage)
+    expect(() => vendingMachine.deposit([10,20])).toThrow(displayMessage)
+
   })
+
 
 // As a customer, I want to add additional money, so that I can use the denominations I have to purchase an item.
 // - Given I have deposited money in the vending machine,
@@ -83,11 +89,34 @@ describe('The vending machine', () => {
 // - then I see the new total on a screen. 
 // - After depositing Rs 100, `deposit(50)` returns `'You have deposited Rs 150'`
 
+  it('keeps track of total amount deposited', () => {
+    const vendingMachine = new Machine();
+    const depositAmount = 50;
+
+    expect(vendingMachine.deposit(depositAmount)).toEqual('You have deposited 50');
+    expect(vendingMachine.deposit(depositAmount)).toEqual('You have deposited 100');
+  })
+
 // As a customer, I want to see a message if my item is unavailable, so that I can make another choice.
 // - Given I am using the vending machine, 
 // - when I enter a code for an item that is unavailable, 
 // - then I see a message that the item is unavailable.
 // - `selectItem(code)` returns `'The item you selected is unavailable'`
+
+  it('has a function selectItem', () => {
+    const vendingMachine = new Machine()
+    expect(typeof vendingMachine.selectItem).toEqual('function')
+  })
+
+  it('says selection is unavailable if item is out of stock', () => {
+    const vendingMachine = new Machine();
+    const displayMessage = 'The item you selected is unavailable';
+    const item = 'Chips';
+
+    expect(vendingMachine.selectItem(item)).toEqual(displayMessage);
+    expect(vendingMachine.selectItem()).toEqual(displayMessage);
+    expect(vendingMachine.selectItem(1)).toEqual(displayMessage);
+  });
 
 // As a customer, I want to see a message if my deposit is insufficient, so that I know to add more money.
 // - Given I have made a choice, 
@@ -95,11 +124,40 @@ describe('The vending machine', () => {
 // - then I see a message telling me how much more to deposit.
 // - `selectItem(code)` returns `'Your deposit is insufficient.  Please add Rs 20 for this item'`
 
+  it('returns an error "Your deposit is insufficient.  Please add X for this item" when funds are inssufecient', () => {
+    const vendingMachine = new Machine()
+
+    const snack = {
+      name: 'macadamia nuts',
+      price: 250
+    }
+
+    // exercise
+    vendingMachine.stock([snack])
+
+    vendingMachine.deposit(100)
+
+    expect(() => vendingMachine.selectItem('macadamia nuts')).toThrow(`Your deposit is insufficient.  Please add 150 for this item`)
+
+  });
+
 // As a customer, I want to receive change, so that I don’t pay more than the item costs.
 // - Given I have made a selection, 
 // - when the item is delivered, 
 // - then I receive correct change (in correct monetary units)
 // - `selectItem(code)` returns an object with the item and an array of bills `{item: 'mints', change: [20, 10]}`
+
+  it('returns correct change alongside a purchased item', () => {
+    const vendingMachine = new Machine();
+    const snack = {
+      name: 'macadamia nuts',
+      price: 310
+    };
+    vendingMachine.stock([snack]);
+    vendingMachine.deposit(250);
+
+    expect(vendingMachine.selectItem('macadamia nuts')).toEqual({item: 'macadamia nuts', change: [50, 10]});
+  });
 
 // As a customer, I want to receive my money back when I push the cancel button, so that I can change my mind.
 // - Given that I have deposited money,
